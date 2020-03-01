44e92fbbfe8f0e135cacfa1370c666258228f8e8
mmaxBA <max29ba@gmail.com>
2020-03-01 11:03:14 +0000

    reverse A01228858

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    // Place your magic here
+    char c, tmp;
+    char* word;
+    int count = 0;
+    word = malloc(4* sizeof(char));
+    while (( c = getchar()) != EOF) {
+        if(c == '\n'){
+            for(int i = 0; i < count/2; i++){
+                tmp = word[i];
+                word[i] = word[count - i - 1];
+                word[count - i - 1] = tmp;
+            }
+
+            printf("%s\n", word);
+            count = 0;
+            free(word);
+            word = malloc(4* sizeof(char));
+        }
+        *(word + count) = c;
+        count++;
+    }
+    free(word);
     return 0;
 }
