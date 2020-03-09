bef8983d45510c210c788b51305dcfa61efb4b53
mmaxBA <max29ba@gmail.com>
2020-03-09 11:31:45 +0000

    Go-slices-maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	var words []string
+	var wordCount map[string]int
+	
+	words = strings.Fields(s)
+	wordCount = make(map[string]int)
+	
+	for _,word := range words {
+             wordCount[word] += 1 
+        }
+
+	return wordCount
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var a [][]uint8
+	a = make([][]uint8, dy)
+	for i := 0; i < len(a); i++ {
+		a[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++{
+			a[i][j] =uint8(i)*uint8(j)/2
+		}
+	}
+	return a
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
