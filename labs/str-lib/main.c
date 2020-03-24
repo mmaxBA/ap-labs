310162b4891b762522b83e2e00cfae0921a17fd0
mmaxBA <max29ba@gmail.com>
2020-03-24 13:39:10 +0000

    str-lib

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char *argv[]) {
+
+    if(argc < 4){
+        printf("You need to pass 3 arguments\n");
+        return -1;
+    }
+
+    char* originalStr = argv[1];
+    char* addStr = argv[2];
+    char* subStr = argv[3];
+    char* newStr = mystradd(originalStr, addStr);
+    char* isSub = mystrfind(originalStr, subStr) ? "yes" : "no";
+
+    printf("Initial Lenght     : %d\n", mystrlen(originalStr));
+    printf("New String         : %s\n", newStr);
+    printf("SubString was found: %s\n", isSub);
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,49 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int mystrlen(char *str){
-    return 0;
+    int len = 0;
+    for( len; str[len] != '\0'; len++);
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int origin_len = mystrlen(origin);
+    int add_len = mystrlen(addition);
+    int new_len = origin_len +add_len;
+    char* str = malloc(sizeof(char)*(new_len));
+    for(int i = 0; i < origin_len; i++){
+        str[i] = origin[i];
+    }
+    for (int j = origin_len; j <new_len ; ++j) {
+        str[j] = addition[j-origin_len];
+    }
+    return str;
 }
 
 int mystrfind(char *origin, char *substr){
+    int origin_len = mystrlen(origin);
+    int sub_len = mystrlen(substr);
+    if (sub_len > origin_len) {
+        return 0;
+    }
+    int found = 0;
+
+    for(int i = 0; i<origin_len-sub_len; i++){
+        int z = 0;
+        for(int j = i; j < i+sub_len; j++){
+            if(origin[j] != substr[z]){
+                found = 0;
+                break;
+            }
+            z++;
+            found = 1;
+        }
+        if(found){
+            return 1;
+        }
+    }
     return 0;
 }
+
